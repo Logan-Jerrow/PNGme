597056806badb846b797e0c12311192c8eb8da18
Logan Jerrow <Logan.Jerrow@gmail.com>
2022-08-20 21:42:18 -0700

    impl ChunkError type

--- src/chunk.rs
+++ src/chunk.rs
@@ -1,11 +1,57 @@
-use crc::{Crc, *};
+use crate::chunk_type::{ChunkType, ChunkTypeError};
+use crc::Crc;
 use std::{
-    fmt::Display,
-    io::{BufReader, Read},
+    error,
+    fmt::{self, Display},
+    io::{self, BufReader, Read},
     str::Utf8Error,
 };
 
-use crate::chunk_type::ChunkType;
+#[derive(Debug)]
+pub enum ChunkError {
+    IoError(std::io::Error),
+    ChuckType(ChunkTypeError),
+    Length { expected: u32, actual: u32 },
+    Crc { expected: u32, actual: u32 },
+}
+
+impl From<ChunkTypeError> for ChunkError {
+    fn from(v: ChunkTypeError) -> Self {
+        Self::ChuckType(v)
+    }
+}
+
+impl From<io::Error> for ChunkError {
+    fn from(v: io::Error) -> Self {
+        Self::IoError(v)
+    }
+}
+
+impl fmt::Display for ChunkError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ChunkError::IoError(e) => write!(f, "encountered an io error while reading bytes: {e}"),
+            ChunkError::ChuckType(e) => e.fmt(f),
+            ChunkError::Length { expected, actual } => write!(
+                f,
+                "length mismatch: given '{expected}' != '{actual}' actual"
+            ),
+            ChunkError::Crc { expected, actual } => {
+                write!(f, "crc mismatch: given '{expected}' != '{actual}' actual")
+            }
+        }
+    }
+}
+
+impl error::Error for ChunkError {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        match self {
+            ChunkError::IoError(e) => Some(e),
+            ChunkError::ChuckType(e) => Some(e),
+            _ => None,
+        }
+    }
+}
 
 #[derive(Debug)]
 pub struct Chunk {
@@ -16,30 +62,34 @@ pub struct Chunk {
 }
 
 impl Chunk {
-    fn new(chunk_type: ChunkType, data: Vec<u8>) -> Chunk {
+    pub fn new(chunk_type: ChunkType, data: Vec<u8>) -> Chunk {
         Chunk { chunk_type, data }
     }
 
     /// A 4-byte unsigned integer giving the number of bytes in the chunk's data field. The length
     /// counts **only the data field**, not itself, the chunk type code, or the CRC. Zero is a valid
     /// length. Although encoders and decoders should treat the length as unsigned, its value must
-    /// not exceed 231 bytes.
-    fn length(&self) -> u32 {
+    /// not exceed 2^31 bytes.
+    pub fn data_length(&self) -> u32 {
         self.data.len() as u32
     }
 
-    fn chunk_type(&self) -> &ChunkType {
+    pub fn size(&self) -> u32 {
+        4 + 4 + self.data_length() + 4
+    }
+
+    pub fn chunk_type(&self) -> &ChunkType {
         &self.chunk_type
     }
 
-    fn data(&self) -> &[u8] {
+    pub fn data(&self) -> &[u8] {
         &self.data
     }
 
     /// A 4-byte CRC (Cyclic Redundancy Check) calculated on the preceding bytes in the chunk,
     /// including the chunk type code and chunk data fields, but **not** including the length field.
     /// The CRC is always present, even for chunks containing no data.
-    fn crc(&self) -> u32 {
+    pub fn crc(&self) -> u32 {
         let chunk: Vec<u8> = self
             .chunk_type
             .bytes()
@@ -48,16 +98,16 @@ impl Chunk {
             .copied()
             .collect();
 
-        Crc::<u32>::checksum(&Crc::<u32>::new(&CRC_32_ISO_HDLC), &chunk)
+        Crc::<u32>::checksum(&Crc::<u32>::new(&crc::CRC_32_ISO_HDLC), &chunk)
     }
 
-    fn data_as_string(&self) -> Result<String, Utf8Error> {
+    pub fn data_as_string(&self) -> Result<String, Utf8Error> {
         std::str::from_utf8(&self.data).map(String::from)
     }
 
-    fn as_bytes(&self) -> Vec<u8> {
+    pub fn as_bytes(&self) -> Vec<u8> {
         [
-            self.length().to_be_bytes().as_slice(),
+            self.data_length().to_be_bytes().as_slice(),
             self.chunk_type.bytes().as_slice(),
             self.data.as_slice(),
             self.crc().to_be_bytes().as_slice(),
@@ -67,7 +117,7 @@ impl Chunk {
 }
 
 impl TryFrom<&[u8]> for Chunk {
-    type Error = Box<dyn std::error::Error>;
+    type Error = ChunkError;
 
     fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
         let mut reader = BufReader::new(value);
@@ -88,30 +138,36 @@ impl TryFrom<&[u8]> for Chunk {
         reader.read_exact(&mut crc)?;
         let crc = u32::from_be_bytes(crc);
 
-        if data.len() as u32 != length {
-            let s = "length mismatch";
-            eprintln!("{s}");
-            let e: Box<dyn std::error::Error> = String::from(s).into();
-            return Err(e);
+        // TODO: length must not exceed 2^31 bytes
+        let data_len = data.len() as u32;
+        if data_len != length {
+            return Err(ChunkError::Length {
+                expected: length,
+                actual: data_len,
+            });
         }
 
         let chunk = Self::new(chunk_type, data);
-
-        if chunk.crc() == crc {
-            println!("MATCH");
-            Ok(chunk)
-        } else {
-            let s = "crc mismatch";
-            eprintln!("{s}");
-            let e: Box<dyn std::error::Error> = String::from(s).into();
-            Err(e)
+        if chunk.crc() != crc {
+            return Err(ChunkError::Crc {
+                expected: chunk.crc(),
+                actual: crc,
+            });
         }
+
+        Ok(chunk)
     }
 }
 
 impl Display for Chunk {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:#?}", self)
+        writeln!(f, "Chunk {{",)?;
+        writeln!(f, "  Length: {}", self.data_length())?;
+        writeln!(f, "  Type: {}", self.chunk_type())?;
+        writeln!(f, "  Data: {} bytes", self.data().len())?;
+        writeln!(f, "  Crc: {}", self.crc())?;
+        writeln!(f, "}}",)?;
+        Ok(())
     }
 }
 
@@ -146,14 +202,14 @@ mod tests {
             .as_bytes()
             .to_vec();
         let chunk = Chunk::new(chunk_type, data);
-        assert_eq!(chunk.length(), 42);
+        assert_eq!(chunk.data_length(), 42);
         assert_eq!(chunk.crc(), 2882656334);
     }
 
     #[test]
     fn test_chunk_length() {
         let chunk = testing_chunk();
-        assert_eq!(chunk.length(), 42);
+        assert_eq!(chunk.data_length(), 42);
     }
 
     #[test]
@@ -197,7 +253,7 @@ mod tests {
         let chunk_string = chunk.data_as_string().unwrap();
         let expected_chunk_string = String::from("This is where your secret message will be!");
 
-        assert_eq!(chunk.length(), 42);
+        assert_eq!(chunk.data_length(), 42);
         assert_eq!(chunk.chunk_type().to_string(), String::from("RuSt"));
         assert_eq!(chunk_string, expected_chunk_string);
         assert_eq!(chunk.crc(), 2882656334);
