661a4cb167ee621c65b8ae9201fdfb3cffe1e722
Logan Jerrow <Logan.Jerrow@gmail.com>
2022-08-20 21:45:10 -0700

    added cli interface with clap

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,86 @@
 #![allow(dead_code)]
 
-use std::fmt::Write;
+use clap::Parser;
+use pngme::chunk_type::ChunkType;
+use std::path::PathBuf;
 
-mod args;
-mod chunk;
-mod chunk_type;
 mod commands;
-mod png;
 
-mod error {
-    use crate::chunk_type;
+// #[derive(Debug, Parser)]
+// struct Args {
+//     #[clap(subcommand)]
+//     encode: Option<Commands>,
+// }
 
-    #[derive(Debug)]
-    pub enum PngError {
-        CT(chunk_type::ChunkTypeError),
-    }
-
-    impl std::error::Error for PngError {}
-    impl std::fmt::Display for PngError {
-        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-            todo!()
-        }
-    }
+#[derive(Debug, Parser)]
+pub enum Commands {
+    Encode(EncodeArgs),
+    Decode(DecodeArgs),
+    Remove(RemoveArgs),
+    Print(PrintArgs),
 }
 
-/// PNG files always contain the following 8 bytes
-///
-/// ['PNG file signature'](http://www.libpng.org/pub/png/spec/1.2/PNG-Rationale.html#R.PNG-file-signature)
-const PNG_8: [u8; 8] = [0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a];
+#[derive(Parser, Debug)]
+pub struct EncodeArgs {
+    #[clap(value_parser)]
+    path: PathBuf,
+
+    #[clap(value_parser)] //= ChunkType::from_str)]
+    chunk_type: ChunkType,
+
+    #[clap(value_parser)]
+    message: String,
+
+    #[clap(value_parser)]
+    output_file: Option<PathBuf>,
+}
+
+#[derive(Parser, Debug)]
+pub struct DecodeArgs {
+    #[clap(value_parser)]
+    path: PathBuf,
+
+    #[clap(value_parser)] //= ChunkType::from_str)]
+    chunk_type: ChunkType,
+}
+
+#[derive(Debug, Parser)]
+pub struct RemoveArgs {
+    #[clap(value_parser)]
+    path: PathBuf,
+
+    #[clap(value_parser)] //= ChunkType::from_str)]
+    chunk_type: ChunkType,
+}
+
+#[derive(Parser, Debug)]
+pub struct PrintArgs {
+    #[clap(value_parser)]
+    path: PathBuf,
+}
 
 fn main() -> anyhow::Result<()> {
-    let png = std::fs::read("blackDragon.png").unwrap();
+    let args = Commands::parse();
 
-    let mut byte = String::from(" ");
-    let mut dec = String::new();
-
-    for b in png[0..8].iter() {
-        write!(&mut byte, "{b:02x?} ").unwrap();
-        write!(&mut dec, "{b:02} ").unwrap();
+    match args {
+        Commands::Encode(args) => commands::encode(args)?,
+        Commands::Decode(_) => todo!(),
+        Commands::Remove(_) => todo!(),
+        Commands::Print(_) => todo!(),
     }
-    let byte = byte.trim_end();
-    let dec = dec.trim();
+    // let png = std::fs::read("blackDragon.png").unwrap();
 
-    println!("{PNG_8:02x?}\n{byte}\n{dec}");
+    // let mut byte = String::from(" ");
+    // let mut dec = String::new();
+
+    // for b in png[0..8].iter() {
+    //     write!(&mut byte, "{b:02x?} ").unwrap();
+    //     write!(&mut dec, "{b:02} ").unwrap();
+    // }
+    // let byte = byte.trim_end();
+    // let dec = dec.trim();
+
+    // println!("{PNG_8:02x?}\n{byte}\n{dec}");
 
     Ok(())
 }
